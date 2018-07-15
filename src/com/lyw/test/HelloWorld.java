b742ace0c359f124c5aa15706c99c1df30fadb68
Administrator <Administrator@windows10.microdone.cn>
2018-07-15 17:00:47 +0800

    啊哒哒哒

--- src/com/lyw/test/HelloWorld.java
+++ src/com/lyw/test/HelloWorld.java
@@ -5,7 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("======================");
-		System.out.println("hello world");
+		System.out.println("======================");
+		System.out.println("hello world   ");
+		System.out.println("hello world   ");
+		
 		
 	}
 }
